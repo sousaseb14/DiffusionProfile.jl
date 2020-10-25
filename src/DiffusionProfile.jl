c820e3b2ec9137d702aff7f1a984c293c7dd1dc3
Sebastian <sousaseb14@gmail.com>
2020-10-25 15:09:04 -0600

    Code now  successfully calculates local diffusion coefficient! Issue with last segment of data giving spikes in the correlation function now fixed. Next step is to modify the main to have it parse windows.

--- src/DiffusionProfile.jl
+++ src/DiffusionProfile.jl
@@ -1,6 +1,5 @@
 module DiffusionProfile
 
-# Write your package code here.
 
 #include
 include("parse_functions.jl")
@@ -11,11 +10,25 @@ using Statistics
 using Plots
 using BenchmarkTools
 
-#import
-obj = readdlm("/Users/seb/Desktop/CO2O2HCO3_graphene_project/wham/HCO3/wham_window1.dat")
-corr = corr_i(obj)
-display(plot(corr[:,1],corr[:,2],color = :blue,linewidth = 4,xlabel = "t (ps)",ylabel = "< δz(t)δz(0) >",label = "< δz(t)δz(0) >", ylims = (-0.001,0.02)))
-savefig("Corr_HCO3_unLP_win1.png")
-#@btime corr_i(obj)
+#import umbrella window time series, calculate <δz(t)δ(0)>.
+obj = readdlm("/Users/seb/Desktop/CO2O2HCO3_graphene_project/wham/HCO3/wham_window15.dat")
+(corr,corr_cut,var0,tCorr) = corr_i(obj,-10.8)
+
+#visualize <δz(t)δ(0)>
+l = length(corr_cut[:,1])
+dash0 = zeros(l,2)
+display(plot(corr_cut[:,1],[corr_cut[:,2],dash0[:,2]],color = [:gray :red],linewidth = [4 1.2],xlabel = "t (ps)",ylabel = "< δz(t)δz(0) >",label = ["< δz(t)δz(0) >"  " 0 "], ylims = (-0.001,0.03),linestyle = [:solid :dashdot]))
+println("< δz(0)δz(0) > = ",var0, " Å^2")
+
+#Integrate correlation function from 0 → "∞"
+int_corr = Nint_corr(corr_cut)
+println("∫< δz(0)δz(0) >dt = ",int_corr," Å^2 ⋅ ps")
+
+D_i = (var0^2/int_corr)*1e-4
+println("Local diffusion coefficient = ",D_i," cm^2/s")
+
+
+#savefig("Corr_HCO3_unLP_win15.png")
+#@btime corr_i(obj,-15.0)
 
 end
--- src/parse_functions.jl
+++ src/parse_functions.jl
@@ -2,52 +2,65 @@
 # for local diffusion coefficient from time series of single observable.
 
 #construct correlator from umbrella window time series.
-function corr_i(W)
+function corr_i(W,mean)
     dt = 2 #2 fs timestep
     tCorr = 3000 #time over which correlations expected to die off.
-    z =  W[:,2] .+ 15.0 # z time series
+    z =  W[:,2] .- mean # z time series (centered)
     nSamp = length(z)
-    corr = zeros(tCorr,2) #initilize array that will contain correlation averges for a given t.
-    norm = zeros(tCorr,1) #array of normalizations for different t.
-    for i in 1:(nSamp-tCorr-1) #loop over all samples (as separate time origins)
+    corr = zeros(tCorr+1,2) #initilize array that will contain correlation averges for a given t.
+    norm = zeros(tCorr+1,1) #array of normalizations for different t.
+    corr[1,1] = 0.0
+    var0 = var(z)
+    corr[1,2] = var0
+    for i in 1:(nSamp) #loop over all samples (as separate time origins)
         tMax = nSamp
-        if i == 1
-            tMax = tCorr
-        end
         if i+tCorr<nSamp
-            tMax = (i-1) + tCorr
+            tMax = i + tCorr
         end
+        idx = 2 #shift index to account for  <δz(0)δ(0)> term.
         for j in (i+1):tMax
             t = (j - i)
-            corr[t,2] = corr[t,2] + z[j]*z[i]
-            corr[t,1] = t*dt/1000 #conv to picoseconds.
-            norm[t] = norm[t] + 1
-            #println(j)
+            corr[idx,2] = corr[idx,2] + z[j]*z[i]
+            corr[idx,1] = t*dt/1000 #conv to picoseconds.
+            norm[idx] = norm[idx] + 1
+            idx = idx + 1
         end
-        if i % tCorr == 0
-            println(norm[1])
-        end
+        #if i % tCorr == 0
+        #    println(norm[2])
+        #end
     end
-    for k = 1:tCorr
+    for k = 2:length(corr[:,1])
         corr[k,2] = corr[k,2]/norm[k]
     end
+
+    #println(corr[length(corr[:,2]),1]," ",corr[length(corr[:,2]),2])
 
     ##################################
     #Below I make the resolution for longer times more coarse, to get rid of some of the noise
     #that might impact numerical integration.
-    corr_cut = zeros(trunc(Int,750+2250/150),2)
+    corr_cut = zeros(trunc(Int,1+750+2250/150),2)
     idx = 1
     for kk = 1:750
         corr_cut[kk,2] = corr[kk,2]
         corr_cut[kk,1] = corr[kk,1]
         idx = idx + 1
     end
-    for jj in 751:150:tCorr
+    for jj in 751:150:(tCorr+1)
         corr_cut[idx,2] = corr[jj,2]
         corr_cut[idx,1] = corr[jj,1]
         idx = idx + 1
     end
-    ##################################
+    ###################################
 
-    return corr_cut
+
+    return corr, corr_cut, var0, tCorr
+end
+
+function Nint_corr(δδ)
+    l = length(δδ[:,2])
+    s = 0
+    for i in 1:(l-1)
+        s = s + (δδ[i+1,1] - δδ[i,1])*0.5*(δδ[i+1,2] + δδ[i,2])
+    end
+    return s
 end
